efe3fc75bca3b26a305419c9a12f49ce31b9c706
Farival <151748088+Farival@users.noreply.github.com>
2024-07-19 02:02:28 +0700

    Add files via upload

--- asset/styles/script.js
+++ asset/styles/script.js
@@ -29,17 +29,42 @@ const slideshow = document.getElementById('slides');
     modal.style.display = "none";
   }
   
- 
-  function toggleNav() {
-    var hamburger = document.querySelector(".hamburger");
-    var navbar = document.getElementById("navbar");
-    hamburger.classList.toggle("open");
-    navbar.style.display = (navbar.style.display === "block") ? "none" : "block";
-  }
-  
-  function closeNav() {
-    var hamburger = document.querySelector(".hamburger");
-    var navbar = document.getElementById("navbar");
+  function toggleMenu(element) {
+    var menu = document.getElementById("menu");
+    var hamburger = document.querySelector('.hamburger');
+    if (menu.style.width === "50%") {
+        menu.style.width = "0";
+        hamburger.classList.remove("open");
+    } else {
+        menu.style.width = "50%";
+        hamburger.classList.add("open");
+    }
+}
+
+function navigateTo(sectionId) {
+    var menu = document.getElementById("menu");
+    var hamburger = document.querySelector('.hamburger');
+    menu.style.width = "0";
     hamburger.classList.remove("open");
-    navbar.style.display = "none";
-  }
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+const angka = document.querySelectorAll('.angka');
+
+angka.forEach((angka) => {
+    angka.addEventListener('click', () => {
+        alert(`Angka yang dipilih: ${angka.textContent}`);
+    });
+});
+
+const isMobile = window.matchMedia("only screen and (max-width: 768px)").matches; //hanya tersedia di mobile//
+
+if (isMobile) {
+    const angka = document.querySelectorAll('.angka, angkap, angkat');
+    
+    angka.forEach((angka) => {
+        angka.addEventListener('click', () => {
+            alert(`Angka yang dipilih: ${angka.textContent}`);
+        });
+    });
+}
